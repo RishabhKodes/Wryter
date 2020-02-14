142dd31fcb500b5b9e3a5cd6e7062ea856c61cce
RishabhKodes <rishabhbhandari6@gmail.com>
2020-02-14 23:39:40 +0530

    comment feature added

--- node_blog/routes/posts.js
+++ node_blog/routes/posts.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest:'./public/images'});
-
+var mongoose =require('mongoose');
 var monk =require('monk');
 const url = 'localhost:27017/nodeblog';
 const db = monk(url);
 var mongo = require('mongodb');
+
+
 router.get('/add', function(req, res, next) {
 
     var categories = db.get('categories');
@@ -17,6 +19,29 @@ router.get('/add', function(req, res, next) {
             'categories':categories
         })        
     })
+    
+});
+
+// router.get('/show/:id', function(req, res, next) {
+//     var posts = db.get('posts');
+
+//     posts.findById(req.params.id,function(err, post){
+//         res.render('show',{
+//             'post':post
+//         });        
+//     });
+    
+// });
+
+
+router.get('/show/:id', function(req, res, next) {
+    var posts = db.get('posts');
+
+    posts.findOne(req.params.id,function(err,post){ 
+        res.render('show',{
+            'post':post
+        });
+    });        
     
 });
 
@@ -67,6 +92,54 @@ router.post('/add',upload.single('mainimage'),function(req, res, next) {
     }
 
 });
+
+
+router.post('/addcomment',function(req, res, next) {
+    //to get form values
+    var name = req.body.name;
+    var email = req.body.email;
+    var body = req.body.body;
+    var postid = req.body.postid;
+    var date = new Date();
+
+    //to check errors
+    var errors =req.validationErrors();
+
+    if(errors){
+        var posts=db.get('posts');
+        posts.findOne(postid, function(err, post){
+            res.render('show',{
+                "errors":errors,
+                "post":post
+            });
+        });
+        
+    }else{
+    var comment = {
+        "name":name,
+        "email":email,
+        "body":body,
+        "commentdate":date
+    }
+     
+    var posts=db.get('posts');
+    posts.update({
+        "_id":postid,   
+    },{
+        $push:{
+            "comments":comment
+        }
+    }, function(err, doc){
+        if(err){
+            throw err;
+        }else{
+            req.flash('success','Comment Added');
+            res.location('/posts/show/'+postid);
+            res.redirect('/posts/show/'+postid);
+        }
+    })
+
+}})
 
 
 module.exports = router;
